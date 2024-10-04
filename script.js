7dbd991fad7d24cc6cbfcad3b19e01bae1b8ab52
ibilpablo <joshjoshdeluvio69@gmail.com>
2024-10-04 22:34:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function akingfunction() {
+    alert('Hi ambererut!');
+
+  
+    document.getElementById("gif").style.display = "block";
+
+
+    setTimeout(function() {
+        alert("Hey Jam, just a little reminder to take a deep breath and know that it’s okay to take things one step at a time. You’ve got this, and I’m here for you!");
+
+        // Open YouTube link after the second alert
+        window.open('https://youtube.com/clip/UgkxIVEZ0EigpD6ME2kb3UhDz_lpZR1y-Hy8?si=o56jubjW5OloObeg', '_blank');
+    }, 100); // 1000 milliseconds = 1 second
+}
+
+document.getElementById("button1").addEventListener("click", akingfunction);
