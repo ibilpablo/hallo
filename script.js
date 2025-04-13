8210c8b3da463d6ae84d75c3ec58bc9659e7d5a0
ibilpablo <joshjoshdeluvio69@gmail.com>
2025-04-13 11:22:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 function akingfunction() {
-    alert('Hi ambererut!');
+    alert('Hi babe!!');
 
   
     document.getElementById("gif").style.display = "block";
 
 
     setTimeout(function() {
-        alert("Hey Jam, just a little reminder to take a deep breath and know that it’s okay to take things one step at a time. You’ve got this, and I’m here for you!");
+        alert("Hey bae, just a little reminder to take a deep breath and know that it’s okay to take things one step at a time. You’ve got this, and I’m here for you!");
 
         // Open YouTube link after the second alert
         window.open('https://youtube.com/clip/UgkxIVEZ0EigpD6ME2kb3UhDz_lpZR1y-Hy8?si=o56jubjW5OloObeg', '_blank');
